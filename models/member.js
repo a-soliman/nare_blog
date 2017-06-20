4746dde49ee6d8453e8d9135fd5a1e3e348b8c59
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-06-20 07:51:35 -0700

    coded the passport login method

--- models/member.js
+++ models/member.js
@@ -24,6 +24,22 @@ var MemberSchema = mongoose.Schema({
 
 var Member = module.exports = mongoose.model('Member', MemberSchema);
 
+module.exports.getMemberById = function(id, callback) {
+    Member.findById(id, callback);
+}
+
+module.exports.getMemberByUsername = function(username, callback) {
+    var query = {username: username};
+    Member.findOne(query, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+    // Load hash from your password DB. 
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+        callback(null, isMatch); 
+    });
+}
+
 module.exports.createMember = function(newMember, callback) {
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(newMember.password, salt, function(err, hash) {
