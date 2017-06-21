c674a5d9f6ddf26b98bc121db1da3dbbd41288cc
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-06-21 07:27:49 -0700

    coded the local stratigy and the passport serialize method

--- routes/members.js
+++ routes/members.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+
 var Member = require('../models/member');
 
 
@@ -16,6 +19,52 @@ router.get('/register', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login',{title: 'Login'});
 });
+
+// === using Passport to login
+
+router.post('/login',
+  passport.authenticate('local', {failurRedirect: '/users/login', failurFlash: 'Invalid username or password'}),
+  function(req, res) {
+    
+    req.flash('success', 'You are now logged in');
+    res.redirect('/');
+
+    res.redirect('/users/' + req.user.username);
+  });
+
+// == passport Serialize
+passport.serializeUser(function(member, done) {
+  done(null, member.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  Member.getMemberById(id, function(err, member) {
+    done(err, member);
+  });
+});
+
+ // local stratigy 
+passport.use(new localStrategy(function(username, password, done) {
+  Member.getMemberByUsername(username, function(err, member) {
+    if(err) throw err;
+    if(!member) {
+      return done(null, false, {message: 'Unknown User'});
+    }
+
+    Member.comparePassword(password, member.password, function(err, isMatch) {
+      if(err) return done(err);
+
+      if(isMatch) {
+        return done(null, member);
+      } else {
+        return done(null, flase, {message: 'Invalid Password'});
+      }
+
+    })
+  })
+}));
+
+
 
 // == handeling the Register form post request
 router.post('/register', function(req, res, next) {
