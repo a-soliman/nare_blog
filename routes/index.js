1e168e6f35783f0d0a32fc02022c185bac9cdfc7
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-06-22 19:21:10 -0700

    coded the route to render the posts from the db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nare_blog');
+
+var db = mongoose.connection;
+
+var Post = require('../models/post');
 
 /* GET home page. */
 router.get('/',ensureAuthenticated , function(req, res, next) {
-  res.render('index', { title: 'Home' });
+
+  var posts = Post
+
+   posts.find({}, {}, function(err, posts) {
+     res.render('index', { title: 'Home', posts: posts });
+   })
+  
 });
 
 function ensureAuthenticated(req, res, next) {
