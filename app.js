e7b40be6b3365a18988be784fa3fb326cfd61c7e
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-06-24 09:41:22 -0700

    debuged the errors and changed multer settings

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
+var expressValidator = require('express-validator')
 var localStrategy = require('passport-local').Strategy;
 var multer = require('multer');
+var upload = multer({dest: './uploads'})
 var flash = require('connect-flash');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
@@ -22,8 +24,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// == Handle file uploads == 
-app.use(multer({dest: './uploads'}))
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
