ac7bf7d998faafd77c107e0b8e0150bd186d1358
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-06-27 11:32:46 -0700

    coded the get req to render a single post && other view to render posts choosen by category

--- routes/posts.js
+++ routes/posts.js
@@ -7,17 +7,38 @@ mongoose.connect('mongodb://localhost/nare_blog');
 var db = mongoose.connection;
 
 var Post = require('../models/post');
+var Category = require('../models/category');
 
-/* GET home page. */
+/* GET postes view page. */
 router.get('/', function(req, res, next) {
 
   var posts = Post
 
    posts.find({}, {}, function(err, posts) {
      res.render('index', { title: 'Posts', posts: posts });
-   })
+   });
   
 });
 
+// Show by category
+router.get('/show/:category', function(req, res, next) {
+    var posts = Post;
+    var categories = Category;
+    
+    posts.find({category: req.params.category}, {}, function(err, posts) {
+        res.render('index', {title: 'Posts', posts: posts})
+    });
+});
+
+// Show Single post
+router.get('/post/:id', function(req, res, next) {
+    var posts = Post;
+    var categories = Category;
+    
+    posts.findById(req.params.id, function(err, post) {
+        res.render('show', {title: 'Posts', post: post})
+    });
+    
+});
 
 module.exports = router;
